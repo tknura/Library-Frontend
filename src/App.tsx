aa7bd9204c8ceecd12c7dd268d8205ac749549e5
Tomasz Knura <tomasz.knura@itsilesia.com>
2021-05-02 11:55:34 +0200

    chore: add base empty project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+function App() {
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App
