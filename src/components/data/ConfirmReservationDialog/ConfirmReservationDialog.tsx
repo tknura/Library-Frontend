8918f47026bde8de27c0c7d8c1a2654207e03103
PatrykLipka <patryk.lipka@itsilesia.com>
2021-06-06 18:03:38 +0200

    fix: even slighter refactor (#5)

--- src/components/data/ConfirmReservationDialog/ConfirmReservationDialog.tsx
+++ src/components/data/ConfirmReservationDialog/ConfirmReservationDialog.tsx
@@ -6,10 +6,10 @@ import { Typography } from '@material-ui/core'
 import * as Styled from './ConfirmReservationDialog.styles'
 
 interface ConfirmReservationModalProps {
-    open: boolean,
-    missingBooks?: string,
-    onConfirm: () => void,
-    onCancel: () => void
+  open: boolean,
+  missingBooks?: string,
+  onConfirm: () => void,
+  onCancel: () => void
 }
 
 const ConfirmReservationModal = ({
@@ -21,44 +21,42 @@ const ConfirmReservationModal = ({
   const { t } = useTranslation()
 
   return (
-    <div>
-      <Modal
-        open={open}
-        onClose={handleCancel}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-      >
-        <Styled.ModalContainer>
-          <Typography variant="h4">
-            {t('screen.cartConfirmationModal.title')}
+    <Modal
+      open={open}
+      onClose={handleCancel}
+      aria-labelledby="simple-modal-title"
+      aria-describedby="simple-modal-description"
+    >
+      <Styled.ModalContainer>
+        <Typography variant="h4">
+          {t('screen.cartConfirmationModal.title')}
+        </Typography>
+        <Styled.ModalDataContainer>
+          <Typography>
+            {missingBooks
+              ? t('screen.cartConfirmationModal.itemsMissing')
+              : t('screen.cartConfirmationModal.orderIsComplete')}
           </Typography>
-          <Styled.ModalDataContainer>
-            <Typography>
-              {missingBooks
-                ? t('screen.cartConfirmationModal.itemsMissing')
-                : t('screen.cartConfirmationModal.orderIsComplete')}
-            </Typography>
-            {!!missingBooks && <Typography>{missingBooks}</Typography>}
-          </Styled.ModalDataContainer>
-          <Styled.ModalButtonsContainer>
-            <Styled.ChoiceButton
-              variant="contained"
-              color="primary"
-              onClick={handleConfirm}
-            >
-              {t('screen.cartConfirmationModal.confirm')}
-            </Styled.ChoiceButton>
-            <Styled.ChoiceButton
-              variant="contained"
-              color="primary"
-              onClick={handleCancel}
-            >
-              {t('screen.cartConfirmationModal.cancel')}
-            </Styled.ChoiceButton>
-          </Styled.ModalButtonsContainer>
-        </Styled.ModalContainer>
-      </Modal>
-    </div>
+          {!!missingBooks && <Typography>{missingBooks}</Typography>}
+        </Styled.ModalDataContainer>
+        <Styled.ModalButtonsContainer>
+          <Styled.ChoiceButton
+            variant="contained"
+            color="primary"
+            onClick={handleConfirm}
+          >
+            {t('screen.cartConfirmationModal.confirm')}
+          </Styled.ChoiceButton>
+          <Styled.ChoiceButton
+            variant="contained"
+            color="primary"
+            onClick={handleCancel}
+          >
+            {t('screen.cartConfirmationModal.cancel')}
+          </Styled.ChoiceButton>
+        </Styled.ModalButtonsContainer>
+      </Styled.ModalContainer>
+    </Modal>
   )
 }
 
