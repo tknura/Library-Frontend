48bc38e2cc8f06c271785d4e7098e6476bc08f3d
Tomasz Knura <tomasz.knura@itsilesia.com>
2021-06-28 10:49:43 +0200

    fix: useHasRole bad return value

--- src/hooks/useHasRole.ts
+++ src/hooks/useHasRole.ts
@@ -2,12 +2,12 @@ import { useUserRoles } from 'components/providers/AuthProvider'
 import { UserRole } from 'types/UserRole'
 
 const useHasRoles = (accessRoles: UserRole[]): boolean => {
-  const userRole = useUserRoles()
-  if (!userRole) {
+  const userRoles = useUserRoles()
+  if (!userRoles) {
     return false
   }
 
-  return accessRoles.some(role => accessRoles?.includes(role))
+  return userRoles.some(role => accessRoles?.includes(role))
 }
 
 export { useHasRoles }
